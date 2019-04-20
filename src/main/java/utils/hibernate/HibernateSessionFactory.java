70595a88036d7bbcbc416ee36f89165e74f66e24
StasQuasilin <stasvasilin@gmail.com>
2019-04-20 07:52:57 +0300

    -

--- src/main/java/utils/hibernate/HibernateSessionFactory.java
+++ src/main/java/utils/hibernate/HibernateSessionFactory.java
@@ -6,6 +6,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 
 import java.util.ArrayList;
 
@@ -14,39 +15,25 @@ import java.util.ArrayList;
  */
 public class HibernateSessionFactory {
 
-    private static SessionFactory sessionFactory = buildSessionFactory();
-    private static final int POOL_SIZE = 5;
-    private static final ArrayList<Session> pool = new ArrayList<>();
-    private static Session session;
-
+    private static final SessionFactory sessionFactory = buildSessionFactory();
 
     protected static SessionFactory buildSessionFactory() {
         final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
                 .configure()
                 .build();
         try {
-            sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+            return new MetadataSources( registry ).buildMetadata().buildSessionFactory();
         } catch (Exception e) {
             StandardServiceRegistryBuilder.destroy( registry );
             throw new ExceptionInInitializerError("Initial SessionFactory failed" + e);
         }
-        return sessionFactory;
     }
 
     public static void init() {
-//        for (int i = 0; i < POOL_SIZE; i++) {
-//            pool.add(sessionFactory.openSession());
-//        }
-
-        session = sessionFactory.openSession();
+        sessionFactory.openSession();
     }
 
     public synchronized static Session getSession() {
-//        if (pool.size() == 0){
-//            return sessionFactory.openSession();
-//        } else {
-//            return pool.remove(0);
-//        }
         return sessionFactory.openSession();
     }
 
@@ -55,9 +42,6 @@ public class HibernateSessionFactory {
     }
 
     public static void shutdown() {
-        for (Session session : pool){
-            session.close();
-        }
         sessionFactory.close();
     }
 
